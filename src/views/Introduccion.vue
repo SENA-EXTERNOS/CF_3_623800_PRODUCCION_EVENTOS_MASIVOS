6f7ef264596ff7b84808f24ce10c8c78fa6beb6b
jorozcop <orojacobo@gmail.com>
2023-04-05 16:05:30 -0500

    video introductorio

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,7 @@
       .col-lg-6.mt-3
        figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/QRmCSFU1nk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/-QRmCSFU1nk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
     
 </template>
